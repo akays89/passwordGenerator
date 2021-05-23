b2382cc47fae99e7a7e6e0cd5b8ee73f9c297447
Amanda Kays <amandkays89@gmail.com>
2021-05-22 22:04:09 -0500

    passLengthPrompt

--- script.js
+++ script.js
@@ -18,7 +18,15 @@ var specialChars = [
 var numericChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lowerCasedChars = [  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCasedChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var chars = '';
+var passw = '';
 
+var passLength = prompt("Please enter your desired password length. The password can have a minimun of 8 and a maximum of 128 characters");
+
+if (passLength === null){}
+else if (isNaN(parseInt(passLength)) || isNaN(passLength) || parseInt(passLength) < 8 || parseInt(passLength) > 128) {
+  alert("This is not a valid password length. Please choose a number between 8 and 128")
+}
 
 
 
