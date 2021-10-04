2017a57c1b40eb522b1658db30f2d07cfa4f67bc
Amanda Kays <amandakays89@gmail.com>
2021-10-04 17:34:43 -0500

    beginning code refactor moved some existing code around and deleted some code to rewrite

--- script.js
+++ script.js
@@ -1,45 +1,34 @@
+let enter;
 
+const specialChars = ["!", "#", "$", "%", "&", "'" ,"*", "+", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@"];
+ 
+const numberChars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const lowerCasedChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCasedChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const choices;
+const password = [];
 
- // Assignment Code
+ 
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-function mixChars(charMix){
-  
-  charMix = charMix.split('');
-  var n = charMix.length;
-  for (var i = n-1; i>0; i--)
-  {
-    var j = Math.floor(Math.random() * (i+1));
-    var tmp = charMix[i];
-    charMix[i] = charMix[j];
-    charMix[j] = tmp;
-  }
-  
-  return charMix.join("");
-  
-  }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 function generatePassword(){
 
-var specialChars = ["!", "#", "$", "%", "&", "'" ,"*", "+", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@"];
- 
-var numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var lowerCasedChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCasedChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var chars = '';
-var passw = '';
 
-var passLength = prompt("Please enter your desired password length. The password can have a minimun of 8 and a maximum of 128 characters");
+
+var enter = parseInt(prompt("Please enter your desired password length. The password can have a minimun of 8 and a maximum of 128 characters"));
 
 if (passLength === null){}
 else if (isNaN(parseInt(passLength)) || isNaN(passLength) || parseInt(passLength) < 8 || parseInt(passLength) > 128) {
@@ -53,7 +42,7 @@ else{
 
   var lowerCaseCon = confirm("Would you like lowercase characters in your password?");
   if(lowerCaseCon){
-    chars = chars + upperCasedChars
+    chars = chars + lowerCasedChars
   }
 
   var specialCon = confirm("Would you like Special characters in your password?");
